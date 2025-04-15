b657f89ea34ee3509bf00c0c8cd4b984f8bd9bc0
Ítalo <italooo.senai@gmail.com>
2025-04-15 11:44:46 -0300

    Adicionar arquivos de projeto.

---
+++ APIEscola/Data/APIEscolaContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIEscola.Data
+{
+    public class APIEscolaContext : IdentityDbContext // Herança do TdentityDbContext para autenticação
+    {
+        public APIEscolaContext(DbContextOptions<APIEscolaContext> options) : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Chama o método OnModelCreating da classe base para a criação das tabelas padrão
+            base.OnModelCreating(modelBuilder);
+            // Configurar a criação de tabela adicionais aqui
+        }
+    }
+}
